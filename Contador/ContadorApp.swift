14b8302bb67fde3c5bacc79e91ea27fb117d7172
doyDynamo <dho2000@hotmail.com>
2026-01-01 19:30:10 -0600

    Initial Commit

---
+++ Contador/ContadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorApp.swift
+//  Contador
+//
+//  Created by Diego Herrera on 2026/01/01.
+//
+
+import SwiftUI
+
+@main
+struct ContadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
